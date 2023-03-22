03ea27ab9821136ec1d35285e8e2057349f5c487
ono0602 <itbizcontact@gmail.com>
2023-03-22 22:21:25 +0900

    script - Merge部分削除

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ $(function () {
   var fadeSpeed1 = 1000;
   var fadeSpeed2 = 2000;
   var fadeSpeed3 = 3000;
-<<<<<<< HEAD
-=======
   var fadeSpeed4 = 4000;
->>>>>>> 221f7fae66b99c16f6463e8e2eaca61d58ff48fc
 
 
   //ページ訪問時の動作
@@ -14,13 +11,10 @@ $(function () {
     .css({ opacity: '0.0' })
     .animate({ opacity: '1' }, fadeSpeed1);
 
-<<<<<<< HEAD
   $('.header-left-smaph')
     .css({ opacity: '0.0' })
     .animate({ opacity: '1' }, fadeSpeed1);
 
-=======
->>>>>>> 221f7fae66b99c16f6463e8e2eaca61d58ff48fc
   $('.fa-envelope')
     .css({ opacity: '0.0' })
     .animate({ opacity: '1' }, fadeSpeed2);
